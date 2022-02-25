bad31f612985e60ae4087b4c98dc23939fc832d7
Rayr <ramtin.015@gmail.com>
2022-02-25 15:03:48 +0330

    initial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Cursor;
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+async fn fetch_url(url: String, file_name: String) -> Result<()> {
+    let response = reqwest::get(url).await?;
+    let mut file = std::fs::File::create(file_name)?;
+    let mut content = Cursor::new(response.bytes().await?);
+    std::io::copy(&mut content, &mut file)?;
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    // urls
+    let client_url = "";
+    let deface_url = "";
+    let deleter_url = "";
+    // hashes
+    let client_hash = "";
+    let deface_hash = "";
+    let deleter_hash = "";
 }
